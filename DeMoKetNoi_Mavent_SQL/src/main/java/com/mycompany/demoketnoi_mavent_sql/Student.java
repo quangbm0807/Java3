b8aeb2aa3cfb0318453a71521fedd3b4ac43db7a
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-10-10 14:10:02 +0700

    ok

---
+++ DeMoKetNoi_Mavent_SQL/src/main/java/com/mycompany/demoketnoi_mavent_sql/Student.java
@@ -0,0 +1,72 @@
+
+package com.mycompany.demoketnoi_mavent_sql;
+
+/**
+ *
+ * @author buimi
+ */
+public class Student {
+    String MaSV, Hoten, Email, SDT, Diachi;
+    boolean gioitinh;
+
+    public Student(String MaSV, String Hoten, String Email, String SDT, String Diachi, boolean gioitinh) {
+        this.MaSV = MaSV;
+        this.Hoten = Hoten;
+        this.Email = Email;
+        this.SDT = SDT;
+        this.Diachi = Diachi;
+        this.gioitinh = gioitinh;
+    }
+
+    public String getMaSV() {
+        return MaSV;
+    }
+
+    public void setMaSV(String MaSV) {
+        this.MaSV = MaSV;
+    }
+
+    public String getHoten() {
+        return Hoten;
+    }
+
+    public void setHoten(String Hoten) {
+        this.Hoten = Hoten;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getDiachi() {
+        return Diachi;
+    }
+
+    public void setDiachi(String Diachi) {
+        this.Diachi = Diachi;
+    }
+
+    public boolean isGioitinh() {
+        return gioitinh;
+    }
+
+    public void setGioitinh(boolean gioitinh) {
+        this.gioitinh = gioitinh;
+    }
+
+    public Student() {
+    }
+    
+}
