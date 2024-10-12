e3dbc86322406ea9db9ecfa4f73296eee97c48f6
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-10-12 13:24:28 +0700

    Ok

---
+++ DuAnMau/src/main/java/com/mycompany/duanmau/DuAnMau.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.duanmau;
+
+/**
+ *
+ * @author buimi
+ */
+public class DuAnMau {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
